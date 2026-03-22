26c15708c3efccd9023011eb687fd286fdb5644f
Henrik Persson <henrik.persson@onelegstudios.com>
2026-03-22 10:18:39 +0100

    fix: add arch tests for command and service provider extensions

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,18 @@
 <?php
 
+use Illuminate\Console\Command;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
 arch('it will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
+
+arch('commands extend the console command base class')
+    ->expect('Onelegstudios\\StarterKitSetup\\Commands')
+    ->toBeClasses()
+    ->toExtend(Command::class)
+    ->toHaveSuffix('Command');
+
+arch('service provider extends package service provider')
+    ->expect('Onelegstudios\\StarterKitSetup\\StarterKitSetupServiceProvider')
+    ->toExtend(PackageServiceProvider::class);
